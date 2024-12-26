5b5c1e9990ca26e869ad5ed5a630dead3241fd0c
Daniewcruz <daniewcruz@gmail.com>
2024-12-26 16:14:51 -0300

    tela bem-vindo  concluidade com sucesso

--- app-senac-aprendizagem/app/index.tsx
+++ app-senac-aprendizagem/app/index.tsx
@@ -1,14 +1,13 @@
-
 import React from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { NameInput } from "../components/inputSaveName" // Importando o novo componente
 import { router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { StyleSheet, Text, Button, View } from "react-native";
+import { StyleSheet, Text, Button, View, Image } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { ButtonGeneric } from "../components/button"; // botão importado
 import { useFonts, LuckiestGuy_400Regular } from "@expo-google-fonts/luckiest-guy";
-import { useFonts as IBMPlexMono_400Regular, IBMPlexMono_700Bold } from "@expo-google-fonts/ibm-plex-mono";
+import { useFonts as IBMPlexMono, IBMPlexMono_400Regular, IBMPlexMono_700Bold, IBMPlexMono_500Medium } from "@expo-google-fonts/ibm-plex-mono";
 
 
 export default function Screen() {
@@ -30,94 +29,157 @@ export default function Screen() {
     LuckiestGuy: LuckiestGuy_400Regular,
     IBMPlexMonoRegular: IBMPlexMono_400Regular,
     IBMPlexMonoBold: IBMPlexMono_700Bold,
+    IBMPlexMonoMedium: IBMPlexMono_500Medium
   });
 
 
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar />
-      <View style={styles.containerScreen}>
+      <StatusBar  />
+      
 
-        <View style={styles.textSuperior}>
-          <Text style={styles.h1Superior}>BEM-VINDO À SUA JORNADA DE APRENDIZADO!</Text>
-        </View>
-
-        <View style={styles.textInfo}>
-          <Text style={styles.p}>
-            Esse app é seu guia no início do programa jovem aprendiz, com informações, ferramenta interativa e atividades para tornar seu aprendizado mais prático.
-          </Text>
-        </View>
-        
-        <View>
-          <Text style={styles.p}>pesonalize sua expêriencia:</Text>
-          <NameInput onSave={saveNameToFile} />
-        </View>
-
-        <Text style={styles.h1Inferior}>Escolha seu avatar:</Text>
-
-        <View>
-          <Text>espaço avatares</Text>
-        </View>
-
-
-        <View>
-          <ButtonGeneric onpress={start} style={styles.button} name="começar" />
-        </View>
+      
+      <View style={styles.cxBemVindo}>
+      <Text style={styles.h1Superior}>BEM-VINDO À SUA JORNADA DE APRENDIZADO!</Text>
       </View>
+      
+      <View style={styles.cxSubtitulo}>
+      <Text style={styles.p}>
+        Esse app é seu guia no início do programa jovem aprendiz, com informações, ferramenta interativa e atividades para tornar seu aprendizado mais prático.
+      </Text>
+      </View>
+      
+
+      <View style={styles.cxGeral}>
+      <Text style={styles.h1Inferior}>
+      pesonalize sua expêriencia:
+      </Text>
+      <View>
+      <NameInput onSave={saveNameToFile}  />
+      </View>
+
+      
+      <Text style={styles.h1Inferior}>Escolha seu avatar:</Text>
+
+      <View style={styles.avatars}>
+        <Text style={styles.p}>Avatar 1</Text>
+        <Text style={styles.p}>Avatar 2</Text>
+        <Text style={styles.p}>Avatar 3</Text>
+      </View>
+    
+      <ButtonGeneric onpress={start} style={styles.button}  name="começar"  />
+      </View>
+
+      <View style={styles.logo}>
+        <Image resizeMode="contain" style={styles.imgLogo}   source={require("../assets/logo-senac.png")} />
+      </View>
+
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  containerScreen:{
-    flex: 1,
-  },
-  textSuperior: {
-    flex: 1,
-    paddingTop:50,
-    padding: 20,
-    borderWidth:1,
-    borderColor: "#044B8B",
-    
-    
-  },
-  textInfo:{
-    flex: 2,
-  },
-  h1Superior: {
-    fontFamily: "LuckiestGuy",
-    fontSize: 22,
-    color: "#044B8B",
-  },
-  h1Inferior: {
-    fontFamily: "LuckiestGuy",
-    fontSize: 22,
-    color: "#F7941D",
-  },
-  p: {
-    color: "#044B8B",
-    fontFamily: "IBMPlexMonoRegular",
-    fontSize: 16,
-    textAlign: "center",
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "space-between",
+      alignItems: "center",
+      paddingTop: 80,
+      backgroundColor: "#fff",
+      
+    },
+    cxBemVindo: {
+      justifyContent: "flex-start",
+      width: '85%',
+      marginBottom: 10,
 
-  },
-  button: {
-    width: '85%',
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: "#F7941D",
-    borderRadius: 30,
-    elevation: 10, // Altere para aumentar a intensidade
-    shadowColor: '#000', // Cor da sombra
-    shadowOffset: { width: 0, height: 4 }, // Direção da sombra
-    shadowOpacity: 0.1, // Transparência da sombra
-    shadowRadius: 5, // Raio de desfoque
-  },
+      
+    },
+
+    cxSubtitulo: {
+      justifyContent: "center",
+      alignItems: "center",
+      width: '90%',
+      marginBottom: 50,
+      
+
+    },
+
+    cxGeral: {
+      justifyContent: "center",
+      alignItems: "flex-start",
+      marginBottom: 20,
+      width: '85%',
+    },
+    h1Superior: {
+      fontFamily: "LuckiestGuy",
+      fontSize: 24,
+      color: "#044B8B",
+      textAlign: "center"
+    },
+    h1Inferior: {
+      fontFamily: "LuckiestGuy",
+      fontSize: 17,
+      color: "#F7941D",
+    },
+    p: {
+      color: "#044B8B",
+      fontFamily: "IBMPlexMonoMedium",
+      fontSize: 16,
+      marginBottom: 10,
+      textAlign: "center",
+      
+      
+      
+    },
+    avatars: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      marginBottom: 20,
+      marginTop: 10,
+      width: '100%',
+      height: 70,
+      borderColor: "#044B8B",
+      backgroundColor : "#fff",
+      borderRadius: 20,
+      padding: 10,
+      elevation: 2.5,
+      shadowColor: "#000",
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+
+    },
+    button: {
+      width: '101%',
+      height: 70,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: "#F7941D",
+      borderRadius: 30,
+      marginTop: 20,
+      marginBottom: 10,
+      elevation: 5,
+      shadowColor: "#044B8B",
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+    
+    },
+    logo: {
+      justifyContent: "center",
+      alignItems: "center",
+      width: '100%',
+      height: 70,
+      backgroundColor : "#044B8B",
+      
+     
+    },
+    imgLogo: {
+      width: 90,
+      height: 100,
+    },
+    
+    
 
 });
--- app-senac-aprendizagem/components/inputSaveName.tsx
+++ app-senac-aprendizagem/components/inputSaveName.tsx
@@ -2,6 +2,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { StyleSheet, TextInput } from "react-native";
 import React, { useState } from 'react';
 
+
 interface NameInputProps {
   onSave: (name: string) => void;
 }
@@ -13,13 +14,18 @@ export const NameInput: React.FC<NameInputProps> = ({ onSave }) => {
     setName(value);
   };
 
+
+   
+    
   return (
     <SafeAreaView style={styles.container}>
       <TextInput
         style={styles.input}
         placeholder="Digite  seu nome..."
+         placeholderTextColor="#114c8099"
         value={name}
         onChangeText={handleChange}
+        
       />
     </SafeAreaView>
   );
@@ -27,14 +33,29 @@ export const NameInput: React.FC<NameInputProps> = ({ onSave }) => {
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+
    
   },
   input: {
-    height: 40,
+    height: 50,
+
     borderColor: '#044B8B',
-    borderWidth: 3,
-    borderRadius: 10,
-    width: 300,
+
+    borderWidth: 1,
+    borderRadius: 15,
+    marginTop: -15,
+    marginBottom: 20,
+
+    paddingLeft: 10,
+
+    width: 329,
+    fontSize: 16,
+    fontFamily: 'IBMPlexMonoMedium',
+
+  color: '#044B8B',
+
   },
 });
 
