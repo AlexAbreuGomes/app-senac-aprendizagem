7401dcd051660e11b910c8d517cb349e63b98757
BezRafael <rafaelb120934@gmail.com>
2024-12-30 10:32:28 -0300

    criação dos componentes de vídeos

---
+++ app-senac-aprendizagem/data/videos.ts
@@ -0,0 +1,25 @@
+import { Video } from "../types/videosTypes";
+
+export const videos: Video[] = [
+    {
+        id: 1,
+        video: 't7Ce5Afri0k',
+        titulo: 'Como Organizar suas Tarefas no Ambiente de Trabalho?',
+        subtitulo: 'De maneira demonstrativa. As principais maneiras.'
+        /*URL Vídeo: https://www.youtube.com/watch?v=t7Ce5Afri0k */
+    },
+    {
+        id: 2,
+        video: '7QAfy96YL88',
+        titulo: 'Como Organizar suas Tarefas no Ambiente de Trabalho?',
+        subtitulo: 'De maneira demonstrativa. As principais maneiras.'
+        /*URL Vídeo: https://www.youtube.com/watch?v=t7Ce5Afri0k */
+    },
+    {
+        id: 3,
+        video: 'hVMxsORwntE',
+        titulo: 'Como Organizar suas Tarefas no Ambiente de Trabalho?',
+        subtitulo: 'De maneira demonstrativa. As principais maneiras.'
+        /*URL Vídeo: https://www.youtube.com/watch?v=t7Ce5Afri0k */
+    }
+]
