456ead531e5ab7821dbe40b4665a04b996aa57b5
AlexAbreuGomes <alex.abreu.softwaredeveloper@gmail.com>
2024-12-31 07:40:39 -0300

    correçao da estrutura de videos

--- app-senac-aprendizagem/components/videoComponent.tsx
+++ app-senac-aprendizagem/components/videoComponent.tsx
@@ -1,36 +1,35 @@
-import {Dimensions, StyleSheet, Text, View} from 'react-native'
+import { Dimensions, StyleSheet, Text, View } from 'react-native'
 import { Video } from "../types/videosTypes";
 
 import { useFonts, LuckiestGuy_400Regular } from "@expo-google-fonts/luckiest-guy";
-import { useFonts as IBMPlexMono_400Regular, IBMPlexMono_700Bold } from "@expo-google-fonts/ibm-plex-mono";
+import { useFonts as IBMPlexMono_400Regular, IBMPlexMono_700Bold} from "@expo-google-fonts/ibm-plex-mono";
 import YoutubeIframe from 'react-native-youtube-iframe';
 
 type Props = {
     data: Video;
 }
 
-export const VideoAprendizagem = ({data}: Props) => {
+export const VideoAprendizagem = ({ data }: Props) => {
 
     useFonts({
         LuckiestGuy: LuckiestGuy_400Regular,
-        IBMPlexMonoRegular: IBMPlexMono_400Regular,
+        IBMPlexMono: IBMPlexMono_400Regular,
         IBMPlexMonoBold: IBMPlexMono_700Bold,
-      });
+    });
 
-    return(
-        <View style={styles.areaVideo}>
-            <View style={styles.videoContainer}>
-                <YoutubeIframe
-                  videoId={data.video}
-                  height={160}
-                  contentScale={0.7}
-                />
-            </View>
+    return (
+
+        <><View style={styles.videoContainer}>
+            <YoutubeIframe
+                videoId={data.video}
+                height={160}
+                contentScale={0.6} />
+        </View>
             <View style={styles.textContainer}>
                 <Text style={styles.textTitulo}>{data.titulo}</Text>
                 <Text style={styles.textSubtitulo}>{data.subtitulo}</Text>
-            </View>
-        </View>
+            </View></>
+
     )
 }
 
@@ -41,40 +40,39 @@ const styles = StyleSheet.create({
         borderRadius: 20,
         borderColor: '#004586',
         width: '100%',
-        height: 230,
+        
     },
 
     videoContainer: {
         width: '100%', // Ajuste conforme necessário
-        height: '70%',
         overflow: 'hidden', // Necessário para aplicar o borderRadius
         borderTopLeftRadius: 18,
         borderTopRightRadius: 18,
         borderWidth: 0,
-        borderColor: 'red'
     },
 
-    textContainer:{
-        borderWidth: 0,
-        borderColor: 'pink',
+    textContainer: {
+        borderWidth: 1,
+        borderBottomRightRadius: 20,
+        borderBottomLeftRadius: 20,
+        borderColor: '#004586',
         width: '100%',
-        height: '30%',
     },
 
-    textTitulo:{
+    textTitulo: {
         backgroundColor: '#004586',
         color: 'white',
-        fontSize: 13,
+        fontSize: 18,
         fontFamily: 'LuckiestGuy',
         textAlign: 'justify',
-        paddingLeft: 5,
-        paddingRight: 5
+        paddingLeft: 10,
+        paddingRight: 10
     },
 
-    textSubtitulo:{
-        fontSize: 12,
-        fontFamily: 'monospace',
-        paddingLeft: 5,
-        paddingRight: 5
+    textSubtitulo: {
+        fontSize: 16,
+        fontFamily: 'IBMPlexMonoRegular',
+        paddingLeft: 10,
+        paddingRight: 10
     }
 })
