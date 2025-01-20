ab1dbe7adb80d391f3e8e7d354f31cc6cc34791d
JeanCaarloc <jeancarlo_fcl@hotmail.com>
2025-01-20 09:55:13 -0300

    Estilizacao pagina detalhes

--- app-senac-aprendizagem/app/detalhes/[id].tsx
+++ app-senac-aprendizagem/app/detalhes/[id].tsx
@@ -6,8 +6,10 @@ import { VideoAprendizagem } from "../../components/videoComponent";
 import { videos } from "../../data/videos";
 import { FontAwesome } from '@expo/vector-icons';
 import { useFonts, LuckiestGuy_400Regular } from "@expo-google-fonts/luckiest-guy";
-import {useFonts as IBMPlexMono,IBMPlexMono_400Regular,IBMPlexMono_700Bold,IBMPlexMono_500Medium} from "@expo-google-fonts/ibm-plex-mono";
+import { useFonts as IBMPlexMono, IBMPlexMono_400Regular, IBMPlexMono_700Bold, IBMPlexMono_500Medium } from "@expo-google-fonts/ibm-plex-mono";
 import { BackButton } from "../../components/backButton";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
 
 export default function Detalhes() {
   const { id } = useLocalSearchParams(); // Pega o ID da URL
@@ -17,12 +19,12 @@ export default function Detalhes() {
     LuckiestGuy: require('@expo-google-fonts/luckiest-guy'),
   });
 
-    useFonts({
-      LuckiestGuy: LuckiestGuy_400Regular,
-      IBMPlexMonoRegular: IBMPlexMono_400Regular,
-      IBMPlexMonoBold: IBMPlexMono_700Bold,
-      IBMPlexMonoMedium: IBMPlexMono_500Medium,
-    });
+  useFonts({
+    LuckiestGuy: LuckiestGuy_400Regular,
+    IBMPlexMonoRegular: IBMPlexMono_400Regular,
+    IBMPlexMonoBold: IBMPlexMono_700Bold,
+    IBMPlexMonoMedium: IBMPlexMono_500Medium,
+  });
 
   // Encontra o conteúdo correspondente ao ID
   const conteudo = conteudosAprendizagem.find((item) => item.id === Number(id));
@@ -37,106 +39,104 @@ export default function Detalhes() {
       <View style={styles.container}>
         <Text style={styles.error}>Conteúdo não encontrado!</Text>
         <Button title="Voltar" onPress={() => router.back()} />
-      </View> 
+      </View>
     );
   }
 
   return (
+    <SafeAreaView>
+      <StatusBar />
+      <ScrollView >
+        <Text style={styles.title}>{conteudo.titulo}</Text>
 
-    <ScrollView contentContainerStyle={styles.container}>
-      <View style={styles.containerAll}>
-      <Text style={styles.title}>{conteudo.titulo}</Text>
-      {videosDoConteudo.length > 0 && (
-        <View style={styles.videosContainer}>
-          <Text style={styles.subTitulo}>Vídeos Relacionados:</Text>
-          {videosDoConteudo.map((video) => (
-            <VideoAprendizagem key={video.id} data={video} />
-          ))}
-        </View>
-      )}
-        
-        <Text style={styles.description}>{conteudo.descricao}</Text>
-        <Text style={styles.text}>{conteudo.texto}</Text>
-
-  
-
-
-        
-        {conteudo.dicas && (
-          <View>
-            <Text style={styles.subtitle}>Dicas:</Text>
-            {conteudo.dicas.map((dica, index) => (
-              <Text key={index} style={styles.dica}>• {dica}</Text>
+        {videosDoConteudo.length > 0 && (
+          <View style={styles.videosContainer}>
+            {videosDoConteudo.map((video) => (
+              <VideoAprendizagem key={video.id} data={video} />
             ))}
           </View>
         )}
+        <View style={styles.container}>
 
-        {/* Renderizar subtítulos, se existirem */}
-        {conteudo.subtitulos && (
-          <View>
-            <Text style={styles.subtitle}>Subtítulos:</Text>
-            {conteudo.subtitulos.map((subtitulo, index) => (
-              <View key={index} style={styles.subsection}>
-                <Text style={styles.subTitle}>{subtitulo.titulo}</Text>
-                <Text style={styles.subText}>{subtitulo.conteudo}</Text>
-                {subtitulo.imagem && (
-                  <Text style={styles.imageDesc}>{subtitulo.imagem.descricao}</Text>
-                )}
-              </View>
-            ))}
+
+          <Text style={styles.description}>{conteudo.descricao}</Text>
+          <Text style={styles.text}>{conteudo.texto}</Text>
+
+          {conteudo.dicas && (
+            <View>
+              <Text style={styles.subtitle}>Dicas:</Text>
+              {conteudo.dicas.map((dica, index) => (
+                <Text key={index} style={styles.dica}>• {dica}</Text>
+              ))}
+            </View>
+          )}
+
+          {/* Renderizar subtítulos, se existirem */}
+          {conteudo.subtitulos && (
+            <View>
+              <Text style={styles.subtitle}>Subtítulos:</Text>
+              {conteudo.subtitulos.map((subtitulo, index) => (
+                <View key={index} style={styles.subsection}>
+                  <Text style={styles.subTitle}>{subtitulo.titulo}</Text>
+                  <Text style={styles.subText}>{subtitulo.conteudo}</Text>
+                  {subtitulo.imagem && (
+                    <Text style={styles.imageDesc}>{subtitulo.imagem.descricao}</Text>
+                  )}
+                </View>
+              ))}
+            </View>
+          )}
+
+          {/* Renderizar imagens, se existirem */}
+          {conteudo.imagens && (
+            <View>
+              <Text style={styles.subtitle}>Imagens:</Text>
+              {conteudo.imagens.map((imagem, index) => (
+                <Text key={index} style={styles.imageDesc}>
+                  {imagem.descricao}
+                </Text>
+              ))}
+            </View>
+          )}
+
+          <View style={styles.congratulations}>
+            <Text style={styles.h1}>Parabéns por concluir o conteúdo!</Text>
           </View>
-        )}
 
-        {/* Renderizar imagens, se existirem */}
-        {conteudo.imagens && (
-          <View>
-            <Text style={styles.subtitle}>Imagens:</Text>
-            {conteudo.imagens.map((imagem, index) => (
-              <Text key={index} style={styles.imageDesc}>
-                {imagem.descricao}
-              </Text>
-            ))}
+
+          <View style={styles.congratulations}>
+            <Text style={styles.h2}>clique no botão abaixo para seguir para o próximo tema e continuar aprendendo!</Text>
           </View>
-        )}
 
-        <View style={styles.congratulations}>
-          <Text style={styles.h1}>Parabéns por concluir o conteúdo!</Text>
+          <TouchableOpacity onPress={() => router.back()} style={styles.button}>
+            <Text style={styles.textButton}>Próximo Tema</Text>
+            <View style={styles.icon}>
+              <FontAwesome name="arrow-right" size={20 as const} color="#F7941D" />
+            </View>
+          </TouchableOpacity>
+
         </View>
-
-
-        <View style={styles.congratulations}>
-          <Text style={styles.h2}>clique no botão abaixo para seguir para o próximo tema e continuar aprendendo!</Text>
-        </View>
-
-        <TouchableOpacity onPress={() => router.back()} style={styles.button}>
-          <Text style={styles.textButton}>Próximo Tema</Text>
-          <View style={styles.icon}>
-            <FontAwesome name="arrow-right" size={20 as const} color="#F7941D" />
-          </View>
-        </TouchableOpacity>
-
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </SafeAreaView>
 
   );
-
-
-
 
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding: 16,
-    backgroundColor: "#fff",
+    padding: 20,
   },
   videosContainer: {
     marginTop: 16,
+    marginBottom: 10,
+    padding:10,
   },
   subTitulo: {
     fontSize: 20,
-    fontWeight: "bold",
     marginBottom: 8,
+    fontFamily: "LuckiestGuy",
+    color: "#044B8B"
   },
   containerAll: {
     flex: 1,
@@ -145,22 +145,31 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 24,
-    marginBottom: 8,
     fontFamily: "LuckiestGuy",
     justifyContent: "center",
     alignItems: "center",
     textAlign: "center",
     alignContent: "center",
+    color: "#044B8B"
   },
   description: {
     fontSize: 16,
     marginBottom: 16,
-    fontFamily: "IBMPlexMonoRegular",
+    fontFamily: "IBMPlexMonoMedium",
+    marginTop: 10,
+    color: "#044B8B",
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "justify",
   },
   text: {
     fontSize: 16,
     marginBottom: 16,
-    fontFamily: "IBMPlexMonoRegular",
+    fontFamily: "IBMPlexMonoMedium",
+    color: "#044B8B",
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "justify",
   },
   subtitle: {
     fontSize: 18,
@@ -170,33 +179,40 @@ const styles = StyleSheet.create({
     alignItems: "center",
     textAlign: "center",
     alignContent: "center",
+    color: "#044B8B"
   },
   dica: {
-    
     fontSize: 16,
     marginLeft: 8,
-    fontFamily: "IBMPlexMonoRegular",
+    fontFamily: "IBMPlexMonoMedium",
+    color: "#044B8B"
   },
   subsection: {
     marginBottom: 12,
     justifyContent: "center",
     alignItems: "center",
-    textAlign: "center",
+    textAlign: "justify",
     alignContent: "center",
+    color: "#044B8B"
   },
   subTitle: {
     fontSize: 18,
     fontFamily: "LuckiestGuy",
+    color: "#044B8B"
   },
   subText: {
     fontSize: 16,
     marginTop: 4,
-    fontFamily: "IBMPlexMonoRegular",
+    fontFamily: "IBMPlexMonoMedium",
+    color: "#044B8B",
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "justify",
   },
   imageDesc: {
     fontSize: 14,
-    fontFamily: "IBMPlexMonoRegular",
-    color: "#555",
+    fontFamily: "IBMPlexMonoMedium",
+    color: "#044B8B",
     marginTop: 4,
   },
   error: {
@@ -242,9 +258,6 @@ const styles = StyleSheet.create({
     alignItems: "center", // Centraliza os filhos horizontalmente
     justifyContent: "center", // Centraliza os filhos verticalmente
     textAlign: "center", // Centraliza o texto dentro do componente
-    borderWidth: 1,
-    borderColor: "white"
-
   },
   h1: {
     fontSize: 16,
