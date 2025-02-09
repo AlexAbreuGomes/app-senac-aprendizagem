37297997d5b7fb5952ab26f25c19acfaa18d1f86
wesllen dhenmyz brito ferreira <wesllen.dhenmyz@outlook.com>
2025-02-09 16:30:59 +0000

    alterado

--- app-senac-aprendizagem/data/videos.ts
+++ app-senac-aprendizagem/data/videos.ts
@@ -3,27 +3,62 @@ import { Video } from "../types/videosTypes";
 export const videos: Video[] = [
     {
         id: 1,
-        video: 'https://www.youtube.com/watch?v=K-ufILCBtxo',
-        titulo: 'Como Organizar suas Tarefas no Ambiente de Trabalho?'
+        video: 'https://youtu.be/h3bb7WJKcAw',
+        titulo: 'Primeiro dia no Curso'
     },
     {
         id: 2,
-        video: 'https://www.youtube.com/watch?v=NlA31v3SZVI',
-        titulo: 'Como Organizar suas Tarefas no Ambiente de Trabalho?'
+        video: 'https://youtu.be/zVUFlmkluw8',
+        titulo: 'Primeiro dia na empresa'
     },
     {
         id: 3,
-        video: 'https://www.youtube.com/watch?v=8NT7oGaRHPU',
-        titulo: 'Como Organizar suas Tarefas no Ambiente de Trabalho?'
+        video: 'https://youtu.be/DQr8v3J6xs4',
+        titulo: 'Mudança de Setor: Adaptação ao Novo'
     },
     {
         id: 4,
-        video: 'https://www.youtube.com/watch?v=XIcnpKo3UMs',
-        titulo: 'Como Organizar suas Tarefas no Ambiente de Trabalho?'
+        video: 'https://youtu.be/07lUJpbq0DM',
+        titulo: 'Experiências com Clientes'
     },
     {
         id: 5,
+        video: 'https://youtu.be/93FbMSmx1kg',
+        titulo: 'Como se dirigir ao cliente'
+    },
+    {
+        id: 6,
+        video: 'https://youtu.be/6PQrwPgp8Lg',
+        titulo: 'Importância do uso de EPI '
+    },
+    {
+        id: 7,
+        video: 'https://youtu.be/TMLOZqWY8dU',
+        titulo: 'Experiência na Aprendizagem da Instrutora'
+    },
+    {
+        id: 8,
+        video: 'https://youtu.be/Soqh7nsZAr4',
+        titulo: 'Jornada de Sucesso Jovens Aprendizes'
+    },
+    {
+        id: 9,
         video: 'https://www.youtube.com/watch?v=XIcnpKo3UMs',
-        titulo: 'Como Organizar suas Tarefas no Ambiente de Trabalho?'
-    }
+        titulo: 'Conecta Aprendiz'
+    },
+    {
+        id: 10,
+        video: 'https://www.youtube.com/watch?v=jZo1SN12EgM',
+        titulo: 'Carteira de Trabalho Digital'
+    },
+    {
+        id: 11,
+        video: 'https://www.youtube.com/watch?v=XIcnpKo3UMs',
+        titulo: 'Verificando CBO e Informações Salarial'
+    },
+    {
+        id: 12,
+        video: 'https://www.youtube.com/watch?v=XIcnpKo3UMs',
+        titulo: 'Playlist para estudo e relaxamento'
+    },
 ];
