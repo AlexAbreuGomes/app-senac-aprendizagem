b8f3442af64919b5fcba314e26fda97e7c8f3a25
wesllen dhenmyz brito ferreira <wesllen.dhenmyz@outlook.com>
2025-02-09 16:39:13 +0000

    alterado

--- app-senac-aprendizagem/data/boxConteudosData.ts
+++ app-senac-aprendizagem/data/boxConteudosData.ts
@@ -5,7 +5,10 @@ export const conteudosAprendizagem: ConteudoAprendizagem[] = [
     id: 1,
     titulo: "Comunicação",
     descricao: "A importância da comunicação e práticas para aprimorar.",
-    texto: "A comunicação é essencial em todas as áreas da vida, ajudando a evitar conflitos e construir relacionamentos.",
+    texto: `O primeiro dia no curso de aprendizagem é um momento de grande expectativa para os jovens aprendizes. É natural sentir um misto de ansiedade e empolgação ao ingressar em um ambiente de formação profissional.
+ Para começar com o pé direito, é essencial estar atento às regras do curso, ouvir atentamente as orientações dos instrutores e manter uma postura profissional desde o início. Esse é o primeiro passo para construir uma trajetória de sucesso na empresa e na carreira.
+Uma das principais dicas para aproveitar bem essa experiência é manter uma atitude proativa. Isso significa participar das atividades, tirar dúvidas e interagir com os colegas de forma respeitosa.
+Além disso, é importante desenvolver a responsabilidade com horários, prazos e compromissos, pois a disciplina adquirida no curso será fundamental para o crescimento profissional. Demonstrar interesse e comprometimento pode abrir portas para futuras oportunidades dentro da empresa.`,
     icon: require("../assets/icons8-communication-96.png"),
     onPress: () => console.log("Comunicação"),
   },
@@ -13,7 +16,9 @@ export const conteudosAprendizagem: ConteudoAprendizagem[] = [
     id: 2,
     titulo: "Educação",
     descricao: "Práticas de ensino e aprendizado eficientes.",
-    texto: "A educação facilita a aprendizagem, promovendo conhecimento, habilidades e valores.",
+    texto: `O primeiro dia na empresa como jovem aprendiz é um momento marcante e repleto de novos desafios. É natural sentir um pouco de nervosismo, mas a chave para um bom início é manter a calma e estar disposto a aprender. Chegar no horário, vestir-se adequadamente e demonstrar uma postura profissional são atitudes essenciais para causar uma boa impressão. Além disso, prestar atenção às regras da empresa e respeitar os colegas e supervisores ajudará a construir uma imagem positiva desde o primeiro dia. 
+Cada empresa tem sua cultura e rotina, e adaptar-se a elas demonstra maturidade e comprometimento. Fazer anotações sobre as tarefas e processos explicados pelos supervisores pode facilitar o aprendizado e evitar dúvidas no futuro. Além disso, manter uma comunicação clara e educada é fundamental, tanto para pedir ajuda quanto para esclarecer instruções. 
+Por fim, é importante lembrar que erros fazem parte do aprendizado, e o mais importante é demonstrar interesse em melhorar. Ser proativo, buscar conhecimento e mostrar disposição para aprender farão a diferença no desenvolvimento profissional.  `,
     icon: require("../assets/icons8-communication-96.png"),
     onPress: () => console.log("Educação"),
   },
@@ -21,7 +26,9 @@ export const conteudosAprendizagem: ConteudoAprendizagem[] = [
     id: 3,
     titulo: "Trabalho em Equipe",
     descricao: "A importância da colaboração e do trabalho conjunto.",
-    texto: "Trabalhar em equipe permite que diferentes habilidades sejam combinadas para alcançar melhores resultados.",
+    texto: `A mudança de setor para um jovem aprendiz pode ser um desafio, mas também uma grande oportunidade de aprendizado. Adaptar-se a um novo ambiente exige paciência, flexibilidade e disposição para aprender. No início, é normal sentir-se um pouco perdido, por isso, observar a rotina do setor, conhecer os novos colegas e entender as expectativas da equipe são passos essenciais.  
+Além disso, é importante encarar a mudança com uma atitude positiva. Cada setor tem suas particularidades e oferece novas experiências que podem ser valiosas para o crescimento profissional.  
+Ser educado, respeitoso e demonstrar interesse em contribuir para o time ajudará na adaptação e na criação de boas relações com os colegas e supervisores. Com o tempo e esforço, o novo setor pode se tornar um e`,
     icon: require("../assets/icons8-communication-96.png"),
     onPress: () => console.log("Trabalho em Equipe"),
   },
@@ -29,7 +36,9 @@ export const conteudosAprendizagem: ConteudoAprendizagem[] = [
     id: 4,
     titulo: "Gestão do Tempo",
     descricao: "Como gerenciar melhor seu tempo e aumentar a produtividade.",
-    texto: "Uma boa gestão do tempo permite que você organize suas tarefas e aumente sua eficiência no dia a dia.",
+    texto: `Lidar com clientes é uma experiência que exige não apenas conhecimento técnico, mas também inteligência emocional. Como jovem aprendiz, é essencial manter a calma, ser educado e ouvir atentamente as necessidades do cliente. Nem sempre as interações serão fáceis, mas saber controlar as emoções e responder com empatia pode fazer toda a diferença. 
+Respirar fundo antes de reagir a situações estressantes e buscar compreender o ponto de vista do cliente ajudam a resolver problemas de forma mais tranquila e profissional. 
+Uma dica importante é sempre manter uma comunicação clara e respeitosa. Caso não saiba responder a alguma dúvida, peça ajuda a um colega ou supervisor, em vez de dar informações erradas. Demonstrar paciência e interesse pelo cliente cria um atendimento mais humanizado e melhora a experiência de todos. Com o tempo, desenvolver inteligência emocional no atendimento contribuirá não apenas para o desempenho no trabalho, mas também para o crescimento pessoal e profissional. `,
     icon: require("../assets/icons8-communication-96.png"),
     onPress: () => console.log("Gestão do Tempo"),
   },
@@ -37,7 +46,93 @@ export const conteudosAprendizagem: ConteudoAprendizagem[] = [
     id: 5,
     titulo: "Resolução de Problemas",
     descricao: "Estratégias para resolver problemas de forma eficaz.",
-    texto: "Resolver problemas exige análise, criatividade e pensamento crítico para encontrar as melhores soluções.",
+    texto: `A forma como o jovem aprendiz se dirige ao cliente faz toda a diferença no atendimento e na imagem da empresa. A comunicação assertiva é essencial para transmitir informações com clareza, respeito e profissionalismo. Falar de maneira educada, utilizando um tom de voz adequado e palavras positivas, ajuda a criar um ambiente de confiança. Além disso, é importante ouvir atentamente o cliente antes de responder, evitando interrupções e demonstrando interesse genuíno na conversa. 
+Uma dica valiosa é sempre usar um vocabulário simples e objetivo, sem termos técnicos que possam confundir o cliente. Caso haja um problema, manter a calma e oferecer soluções de forma clara e educada ajuda a evitar conflitos.  
+Se não souber a resposta para alguma dúvida, ser sincero e buscar ajuda com um supervisor é a melhor opção. Com prática, a comunicação assertiva se torna uma ferramenta poderosa para um atendimento de qualidade e para o crescimento profissional do aprendiz. `,
+    icon: require("../assets/icons8-communication-96.png"),
+    onPress: () => console.log("Resolução de Problemas"),
+  },
+  {
+    id: 6,
+    titulo: "Resolução de Problemas",
+    descricao: "Estratégias para resolver problemas de forma eficaz.",
+    texto: `O uso de Equipamentos de Proteção Individual (EPI) é fundamental para garantir a segurança dos jovens aprendizes no ambiente de trabalho. Esses equipamentos, como luvas, capacetes, óculos de proteção e aventais, ajudam a prevenir acidentes e proteger contra riscos específicos de cada função.  
+É essencial que o aprendiz utilize corretamente os EPIs fornecidos pela empresa e siga todas as orientações de segurança. Negligenciar o uso desses equipamentos pode colocar em risco sua saúde e comprometer sua permanência na empresa. 
+Além de usar os EPIs adequadamente, o jovem aprendiz deve manter os equipamentos em bom estado, limpando e armazenando-os corretamente após o uso. Caso algum EPI apresente defeito ou esteja desgastado, é importante informar o supervisor imediatamente para garantir a substituição. A segurança no trabalho é uma responsabilidade de todos, e desenvolver esse hábito desde cedo contribui para uma carreira mais segura e profissional. 
+Fonte: Norma Regulamentadora 06`,
+    icon: require("../assets/icons8-communication-96.png"),
+    onPress: () => console.log("Resolução de Problemas"),
+  },
+  {
+    id: 7,
+    titulo: "Resolução de Problemas",
+    descricao: "Estratégias para resolver problemas de forma eficaz.",
+    texto: `Minha trajetória profissional começou como a de muitos dos meus alunos: fui uma jovem aprendiz. Lembro-me bem da ansiedade do primeiro dia, das dúvidas sobre o mercado de trabalho e do desafio de conciliar estudo e prática. 
+O tempo passou, e hoje estou do outro lado, como instrutora. A experiência como aprendiz me permite enxergar cada aluno com empatia, entendendo seus desafios e inseguranças. Sei que o aprendizado vai além do conteúdo técnico; envolve desenvolver confiança, postura profissional e habilidades que farão diferença em suas carreiras.
+Cada turma me ensina algo novo, e minha missão é ser um guia nesse processo, assim como um dia tive instrutores que me inspiraram. Ver o crescimento dos meus alunos e saber que posso contribuir para seu futuro é a maior realização da minha jornada.`,
+    icon: require("../assets/icons8-communication-96.png"),
+    onPress: () => console.log("Resolução de Problemas"),
+  },
+  {
+    id: 8,
+    titulo: "Resolução de Problemas",
+    descricao: "Estratégias para resolver problemas de forma eficaz.",
+    texto: `Concluir o curso de aprendizagem é mais do que encerrar uma etapa; é o marco de um começo repleto de possibilidades. Para aqueles que chegaram até aqui, cada esforço, cada desafio enfrentado e cada nova habilidade desenvolvida são provas de que a dedicação e a perseverança valem a pena. 
+Vocês não são apenas aprendizes que passaram por um curso, mas jovens que carregam consigo histórias de superação, crescimento e transformação.
+Ao longo dessa jornada, aprenderam não apenas sobre técnicas e processos, mas também sobre a importância da ética, do trabalho em equipe e da resiliência diante dos desafios. 
+Esses valores, aliados ao conhecimento adquirido, são ferramentas poderosas para enfrentar o mercado de trabalho e construir um futuro promissor.
+Para aqueles que ainda estão no início ou no meio do caminho, inspirem-se nesses exemplos de sucesso. Cada passo dado hoje é um tijolo na construção de uma carreira sólida. Aproveitem cada oportunidade para aprender, mesmo nos momentos mais desafiadores. Lembrem-se: o aprendizado é uma ponte que conecta o presente aos sonhos do futuro.`,
+    icon: require("../assets/icons8-communication-96.png"),
+    onPress: () => console.log("Resolução de Problemas"),
+  },
+  {
+    id: 9,
+    titulo: "Resolução de Problemas",
+    descricao: "Estratégias para resolver problemas de forma eficaz.",
+    texto: `No universo da aprendizagem, a criatividade e a inovação são ferramentas poderosas para inspirar e engajar. Foi com essa visão que dois aprendizes, Wivilly Martins de Oliveira e João Victor da Silva Galdino, uniram seus talentos e ideias para criar uma música que chama-se "Conecta Aprendiz". Tomaram como base a melodia e letra da música “Aquarela”, do cantor Toquinho. 
+Com letra criativa e melodia cativante, "Conecta Aprendiz" busca não apenas entreter, mas também motivar. Os aprendizes exploraram experiências vividas por eles no programa de aprendizagem, transformando desafios e conquistas em versos que refletem a realidade dos jovens que dão os primeiros passos no mercado de trabalho.
+A música não é apenas uma expressão artística; é um convite para que os novos aprendizes se sintam acolhidos e motivados a aproveitar cada momento dessa experiência transformadora. Ao conectar música e aprendizado, Wivilly e João Vitor mostraram que é possível inspirar outros a enxergar o curso não apenas como uma etapa profissional, mas como uma oportunidade de crescimento pessoal.
+Com "Conecta Aprendiz", eles deixam uma mensagem clara: aprender pode ser desafiador, mas também pode ser leve, colaborativo e cheio de boas surpresas. Afinal, juntos, todos podem construir um futuro melhor, onde a criatividade e a dedicação são as chaves do sucesso..`,
+    icon: require("../assets/icons8-communication-96.png"),
+    onPress: () => console.log("Resolução de Problemas"),
+  },
+  {
+    id: 10,
+    titulo: "Resolução de Problemas",
+    descricao: "Estratégias para resolver problemas de forma eficaz.",
+    texto: `A Carteira de Trabalho Digital é a versão eletrônica da tradicional Carteira de Trabalho e Previdência Social (CTPS) em papel. Ela foi implementada para modernizar o acesso às informações trabalhistas, permitindo que trabalhadores consultem seus contratos, salários, férias e anotações diretamente pelo aplicativo ou site do Governo Federal. 
+Integrada ao eSocial, a carteira digital elimina a necessidade do documento físico para novas contratações, tornando os processos mais ágeis e seguros tanto para empregadores quanto para empregados.
+O eSocial (Sistema de Escrituração Digital das Obrigações Fiscais, Previdenciárias e Trabalhistas) é um sistema do governo federal que unifica o envio de informações trabalhistas, previdenciárias e fiscais das empresas em uma plataforma digital. 
+Ele substitui diversos formulários e obrigações acessórias, facilitando a fiscalização e garantindo maior transparência nas relações de trabalho. Com o eSocial, empregadores comunicam eventos como admissões, demissões, folhas de pagamento e afastamentos de forma padronizada, reduzindo erros e burocracias. 
+Fonte: Gov.br`,
+    icon: require("../assets/icons8-communication-96.png"),
+    onPress: () => console.log("Resolução de Problemas"),
+  },
+  {
+    id: 11,
+    titulo: "Resolução de Problemas",
+    descricao: "Estratégias para resolver problemas de forma eficaz.",
+    texto: `A CBO (Classificação Brasileira de Ocupações), é uma tabela criada pelo Ministério do Trabalho e Emprego do Brasil para organizar e classificar as diversas profissões existentes no mercado de trabalho. 
+Ela descreve as ocupações de acordo com suas características, funções e responsabilidades, ajudando a identificar o perfil de cada profissão. 
+A partir de 1º de janeiro de 2025, o salário mínimo no Brasil foi reajustado para R$ 1.518,00. Com base nesse valor, o salário-hora é de R$ 6,90. O salário deve ser anotado uniformemente na CTPS, no contrato de aprendizagem e na ficha de registro, identificando se o valor é mensal ou por hora. Para jovens aprendizes, a remuneração é calculada de acordo com a carga horária semanal.
+Escolhendo fixar o salário por hora, deve-se aplicar a fórmula abaixo a cada mês para o cálculo do valor a ser pago mensalmente:
+Salário Mensal = (salário-hora x horas trabalhadas por semana x número de semanas no mês x 7) / 6
+Considerando um mês de 4,4285 semanas (31 dias), e o aprendiz com uma jornada de 20 horas semanais (4 horas por dia), o cálculo fica:
+
+Salário = 6,90 x 20 x 4,4285 x 7 ÷ 6 = R$ 712,98 (Salário Bruto Aprendiz).
+
+Fonte: Portal Emprega Brasil`,
+    icon: require("../assets/icons8-communication-96.png"),
+    onPress: () => console.log("Resolução de Problemas"),
+  },
+  {
+    id: 12,
+    titulo: "Resolução de Problemas",
+    descricao: "Estratégias para resolver problemas de forma eficaz.",
+    texto: `Para tornar sua rotina de aprendizado mais leve e produtiva, o link que direciona para o Spotify traz uma seleção especial de playlist sugerida e pensada para atender às diferentes necessidades do seu dia a dia. Fazendo o cadastro na plataforma, você consegue escutar a playlist do Conecta Jovem de forma gratuita.
+Quando o assunto é estudo, a música pode ser uma grande aliada para manter o foco, aumentar a concentração e estimular a criatividade. Por
+isso, incluímos trilhas sonoras instrumentais, e outras opções ideais para criar um ambiente calmo e inspirador enquanto você mergulha nos conteúdo do curso, trabalho e vida pessoal.
+Aproveite essas playlists para encontrar o ritmo ideal para cada momento. Seja estudando ou relaxando, a música pode transformar sua experiência e ajudar a manter o equilíbrio necessário para seguir em frente com disposição e motivação. Dê o play e aproveite!`,
     icon: require("../assets/icons8-communication-96.png"),
     onPress: () => console.log("Resolução de Problemas"),
   },
