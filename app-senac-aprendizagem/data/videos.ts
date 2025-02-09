0bcad210fd3d8c5d1742dc88c58b1f462f9f221b
wesllen dhenmyz brito ferreira <wesllen.dhenmyz@outlook.com>
2025-02-09 16:04:08 +0000

    change

--- app-senac-aprendizagem/data/videos.ts
+++ app-senac-aprendizagem/data/videos.ts
@@ -4,31 +4,26 @@ export const videos: Video[] = [
     {
         id: 1,
         video: 'https://www.youtube.com/watch?v=K-ufILCBtxo',
-        titulo: 'Como Organizar suas Tarefas no Ambiente de Trabalho?',
-        subtitulo: 'De maneira demonstrativa. As principais maneiras.'
+        titulo: 'Como Organizar suas Tarefas no Ambiente de Trabalho?'
     },
     {
         id: 2,
         video: 'https://www.youtube.com/watch?v=NlA31v3SZVI',
-        titulo: 'Como Organizar suas Tarefas no Ambiente de Trabalho?',
-        subtitulo: 'De maneira demonstrativa. As principais maneiras.'
+        titulo: 'Como Organizar suas Tarefas no Ambiente de Trabalho?'
     },
     {
         id: 3,
         video: 'https://www.youtube.com/watch?v=8NT7oGaRHPU',
-        titulo: 'Como Organizar suas Tarefas no Ambiente de Trabalho?',
-        subtitulo: 'De maneira demonstrativa. As principais maneiras.'
+        titulo: 'Como Organizar suas Tarefas no Ambiente de Trabalho?'
     },
     {
         id: 4,
         video: 'https://www.youtube.com/watch?v=XIcnpKo3UMs',
-        titulo: 'Como Organizar suas Tarefas no Ambiente de Trabalho?',
-        subtitulo: 'De maneira demonstrativa. As principais maneiras.'
+        titulo: 'Como Organizar suas Tarefas no Ambiente de Trabalho?'
     },
     {
         id: 5,
         video: 'https://www.youtube.com/watch?v=XIcnpKo3UMs',
-        titulo: 'Como Organizar suas Tarefas no Ambiente de Trabalho?',
-        subtitulo: 'De maneira demonstrativa. As principais maneiras.'
+        titulo: 'Como Organizar suas Tarefas no Ambiente de Trabalho?'
     }
 ];
