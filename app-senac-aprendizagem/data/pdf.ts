e0347203575ba5f0d8138d8fd8bf882ec2b266f6
Alex de Abreu <alex.abreu.softwaredeveloper@gmail.com>
2025-02-19 11:10:16 -0300

    troca do link das atas

--- app-senac-aprendizagem/data/pdf.ts
+++ app-senac-aprendizagem/data/pdf.ts
@@ -3,14 +3,14 @@ import { PdfData } from "../types/pdfTypes";
 export const PDF : PdfData[] = [
     {
         id: 1,
-        url: 'https://drive.google.com/file/d/1SSLpACOkgI7vF0auTcgyJb0v6hs01oIp/view?usp=sharing',
+        url: 'https://drive.google.com/file/d/1kcCY_gMk4hVGWOn0Z5080hvRYVHPq2tu/view?usp=sharing',
     },
     {
         id: 2,
-        url: 'https://drive.google.com/file/d/1HTDm-Py7KMfLG9f3ZNzWWtiu0KCPdEJI/view?usp=sharing',
+        url: 'https://drive.google.com/file/d/1tpnx-AWwfgg5tec_ZK26OAK9RfnvzqRZ/view?usp=sharing',
     },
     {
         id: 3,
-        url: 'https://drive.google.com/file/d/14x4y0Rt_oS7kbPZgwHXq6s-9cFKweuxR/view?usp=sharing',
+        url: 'https://drive.google.com/file/d/1D48MGZzi-XzkjVth8mlmastX0t9nMslg/view?usp=sharing',
     }
 ]
